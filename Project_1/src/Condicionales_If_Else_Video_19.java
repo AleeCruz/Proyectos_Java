e4363fdaf8a7d6fe1824d6d0bd50550cfd0c3d6a
Alexander <acruz@fi.uba.ar>
2026-01-21 15:30:37 -0300

    Ejercicio 18 de java completado

---
+++ Project_1/src/Condicionales_If_Else_Video_19.java
@@ -0,0 +1,14 @@
+public class Condicionales_If_Else_Video_19 {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Project_1/src/Operadores_Logicos_Video_18.java
@@ -0,0 +1,22 @@
+public class Operadores_Logicos_Video_18 {
+    public static void main(String[] args) {
+
+        boolean a = true, b=false;
+
+
+        //Operadores logicos and
+
+        System.out.println(a && b);
+
+        //Operador logico or
+        System.out.println( a || b);
+
+        //Operador Logico not
+        System.out.println(!a);
+
+        //Operador Logico Xor
+        System.out.println(a^b);
+
+
+    }
+}
