b36f1d1e39961a19e8f813fe219db96f3e0bf029
Alexander <acruz@fi.uba.ar>
2026-01-20 22:18:10 -0300

    Se agregaron ejercicio de java desde el ide de eclipse

---
+++ proyectos_Java_eclipse/PrimerosPasos/src/UsoDeVariables.java
@@ -0,0 +1,19 @@
+
+public class UsoDeVariables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte edad;
+		/*Se usaron los comentarios necesarios para para poder hablar sobre el codigo 
+		 * que vamos a desarrollar en
+		 * este caso se trata de ejemplos de juguete ajio ajio*/
+		
+		edad = 17;
+		
+		System.out.println(edad);
+		edad = 25;
+		
+		System.out.println(edad);
+	}
+
+}
