f505f09676cf70d2cc6cbfe682f360e643b27812
Alexander <acruz@fi.uba.ar>
2026-01-24 16:55:10 -0300

    Agregado: Se ejercito un poco sobre los pilares fundamentales de la programacion orientada a objetos

---
+++ proyectos_Java_eclipse/PrimerosPasos/src/Coche.java
@@ -0,0 +1,41 @@
+
+/*
+ * Se vio los conceptos de programacion Orientado a objetos sobre CLases y Objetos
+ * podemosdecir que la programacion orientada a objetos  tiene pilares fundamentales
+ * 
+ * Los principales 
+ * 
+ * 		->Clases
+ * 		-<Objetos
+ * 
+ * */
+
+
+public class Coche {
+	
+	//Atributos de la clase coche 
+	
+	double andho;
+	double alto;
+	double peso;
+	String color;
+	
+	//AHora vamos a crear metodos de la clase coche 
+	
+	void arrancar() {
+		
+	}
+	
+	void frenar() {
+		
+	}
+	
+	
+	void girar() {
+		
+	}
+	
+	
+	
+
+}
---
+++ proyectos_Java_eclipse/PrimerosPasos/src/UsoCoches.java
@@ -0,0 +1,34 @@
+
+public class UsoCoches {
+
+	public static void main(String[] args) {
+		
+		
+		/*Acabamos de ejercitar los conceptos del video  27 y 28 
+		 * Se crearon 2 archivos para la ejercitacion del uso de las clases 
+		 * una clase de tipo Coche y otra clase donde se crearan y usaran todos las instancias de la clase 
+		 * cohce*/
+		
+		
+		
+		Coche renault = new Coche();
+		
+		
+		renault.andho = 1523.3;
+		
+		renault.arrancar();
+		
+		
+		
+		Coche mazda = new Coche();
+
+		
+		mazda.color = "Azul";
+		
+		mazda.girar();
+		
+		
+		
+	}
+
+}
