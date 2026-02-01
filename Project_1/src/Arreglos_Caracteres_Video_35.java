67018c223002b9499b97cd85fce1edbc755d55ab
Alexander <acruz@fi.uba.ar>
2026-02-01 18:25:11 -0300

    Agregado: Ejercicio 35 de lenguaje Java

---
+++ Project_1/src/Arreglos_Caracteres_Video_35.java
@@ -0,0 +1,44 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Arreglos_Caracteres_Video_35 {
+    public static void main(String[] args) {
+
+        //Veremos la declaracion e inicializacion de los productos
+
+        String[] productos = new String[5];
+        productos[0]="manzanas";
+        productos[1]="uvas";
+        productos[2]="peras";
+        productos[3]="fresas";
+        productos[4]="kiwis";
+
+        //Ahora vamos a visualizarlas por pantalla de la siguiente manera
+
+        System.out.println("\n-----------Productos-----------");
+        System.out.println("\n-----------Producto en orden establecido-------");
+        System.out.println(productos[0]);
+        System.out.println(productos[1]);
+        System.out.println(productos[2]);
+        System.out.println(productos[3]);
+        System.out.println(productos[4]);
+
+
+
+        //Ahora vamos a ordenarlos en orden alfabetico de la siguiente manera
+
+        Arrays.sort(productos);
+        System.out.println("\n\n-----------Productos en orden Alfabetico--------");
+        System.out.println(productos[0]);
+        System.out.println(productos[1]);
+        System.out.println(productos[2]);
+        System.out.println(productos[3]);
+        System.out.println(productos[4]);
+
+
+
+
+
+
+    }
+}
---
+++ Project_1/src/Arreglos_Enteros_Video_35.java
@@ -0,0 +1,53 @@
+public class Arreglos_Enteros_Video_35 {
+    public static void main(String[] args) {
+        //declaracion e inicializacion de una variable de enteros
+
+        //Forma 1
+        int [] numeros = new int[5];
+
+        //Asignacion de valores a una variabe
+        numeros[0] =12;
+        numeros[1] =80;
+        numeros[2] =32;
+        numeros[3] =73;
+        numeros[4] =2;
+
+        //Visualizacion de valores de un arreglo
+        System.out.println(numeros[0]);
+        System.out.println(numeros[1]);
+        System.out.println(numeros[2]);
+        System.out.println(numeros[3]);
+        System.out.println(numeros[4]);
+
+        //Podriamos tranquilamente asignar los valores de un arreglo a distintas variables
+
+        int a = numeros[0];
+        int b = numeros[1];
+        int c = numeros[2];
+        int d = numeros[3];
+        int e = numeros[4];
+
+        System.out.println("\nSalida de datos por pantalla");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+
+
+
+
+
+
+        //Forma 2
+        int[] valores = {12,43,65,78,21};
+        System.out.println("\nSalida de datos por pantalla de un arreglo");
+        System.out.println(valores[3]);
+
+
+
+
+
+    }
+}
