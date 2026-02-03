b7248cceed8dce4bf7bd608896a4e22212ff435e
Alexander <acruz@fi.uba.ar>
2026-02-02 23:44:46 -0300

    Agregado: Se agrego el ejercicio 37 de Java

---
+++ Project_1/src/Arrays_Combinacionales_Video_37.java
@@ -0,0 +1,16 @@
+public class Arrays_Combinacionales_Video_37 {
+    public static void main(String[] args) {
+         String[] productos  = {"Martillo","Destornillador","Taladro","Llave inglesa","Pinzas"};
+         double[]precios = {12.50,10.20,200.00,130.50,20.35};
+
+
+         for (int i=0;i<productos.length;i++){
+
+             if (precios[i]<100){
+                 System.out.println("Producto "+(i+1)+" "+productos[i]+": $"+precios[i]+" se le aplicaran impuestos");
+             }else{
+                 System.out.println("Producto "+(i+1)+" "+productos[i]+": $"+precios[i]+" No se le aplicaran impuestos");
+             }
+         }
+    }
+}
