cd63e040750a5ac3a4cc1efcdc2d0ea88e22ad56
Alexander <acruz@fi.uba.ar>
2026-02-07 16:24:58 -0300

    Agregado: Se agrego un nuevo proyecto de POO

---
+++ POO/src/Automovil.java
@@ -0,0 +1,25 @@
+//Teoria y practica de creacion de clases e instancias de un objeto
+
+public class Automovil {
+    String marca = "Honda";
+    String modelo = "CRV";
+    int anio = 2024;
+    int cant_ruedas = 4;
+    boolean camara =true;
+
+
+
+
+    static void main() {
+        //Vamos a instaciar un objeto auto a partir de la clase
+
+        Automovil  auto_1 = new Automovil();
+
+        System.out.println("La marca del automovil es: "+auto_1.marca);
+        System.out.println("El modelo del automovil es: "+auto_1.modelo);
+        System.out.println("EL año del automovil es: "+auto_1.anio);
+        System.out.println("La cantidad de ruedas es: "+auto_1.cant_ruedas);
+        System.out.println("Tiene camara: "+auto_1.camara);
+    }
+
+}
